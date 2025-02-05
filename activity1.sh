e52820963c642ee0aacb27c09427b2a52bc1bf59
Angelica Toquero <152822754+AngelicaToquero@users.noreply.github.com>
2025-02-05 01:40:41 +0000

    //first activity in bash

---
+++ activity1.sh
@@ -0,0 +1,15 @@
+# NAME="Student"
+# AGE="YourAge"
+# PROGRAM="Program"
+
+echo "Enter Name:"
+read NAME
+
+echo "Enter Your Age:"
+read AGE
+
+echo "Enter Program:"
+read PROGRAM
+
+echo "Hello, $NAME. 
+This is your age, $AGE, and your program is $PROGRAM."
